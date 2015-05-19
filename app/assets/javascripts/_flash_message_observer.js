b190d865d397f00da53f441eeafa515324d88dd5
Rob McGrail <rob.mcgrail@gmail.com>
2015-05-19 13:15:47 +1200

    Tidyup of flash messages

--- app/assets/javascripts/_flash_message_observer.js
+++ app/assets/javascripts/_flash_message_observer.js
@@ -1,10 +1,14 @@
 $(function() {
+  $('.alert.close').on('click', function(e) {
+    $(this).parent().hide();
+  });
+
   $(document).ajaxComplete(function(event, request) {
     var msg = request.getResponseHeader('X-Message-Present');
     if (msg) {
       $.get( "/flash", function( data ) {
         $('#flash_messages_container').prepend(data);
-        $('.alert .close').on('click', function(e) {
+        $('.alert.close').on('click', function(e) {
           $(this).parent().hide();
         });
       });
